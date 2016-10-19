442d6b9e8fab9152addb923622f76afd9c354b9d
Nudes <Nudes>
2016-10-19 15:30:53 -0300

    já está listando as coisas, falta mostrar os detalhes em específico

--- www/js/app.js
+++ www/js/app.js
@@ -1,9 +1,71 @@
-// Ionic Starter App
+// Ionic sisma App
 
 // angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// 'sisma' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('sisma', ['ionic'])
+.controller('DemandasController', function($scope, $http) {
+
+$http.get('http://10.21.0.137/20131011110380/api/demanda').
+    then(function(response) {
+        $scope.demandas = response.data;
+    });
+})
+.controller('LocaisController', function($scope, $http){
+
+  $http.get('http://10.21.0.137/20131011110380/api/local').
+    then(function(response) {
+      $scope.locais = response.data;
+    });
+})
+.controller('OcorrenciasController', function($scope, $http){
+
+  function setDemanda(demandaId, id)
+  {
+        $http.get('http://10.21.0.137/20131011110380/api/demanda').
+            then(function(responsedemanda) {
+                $scope.demandas = responsedemanda.data;
+                var demanda = $.grep($scope.demandas, function(e){ return e.id == demandaId; });
+                $scope.ocorrencias[id].demanda = demanda;
+                console.log($scope.ocorrencias[id].id);
+                console.log($scope.ocorrencias[id].demanda);
+            });
+  }
+
+  function setLocal(localId, id)
+  {
+    $http.get('http://10.21.0.137/20131011110380/api/local').
+        then(function(responselocal) {
+            $scope.locais = responselocal.data;
+            var local = $.grep($scope.locais, function(e){ return e.id == localId; });
+            $scope.ocorrencias[id].local = local;
+            console.log($scope.ocorrencias[id].id);
+            console.log($scope.ocorrencias[id].local);
+        });
+  }
+
+
+  $http.get('http://10.21.0.137/20131011110380/api/ocorrencia').
+  then(function(response) {
+    $scope.ocorrencias = response.data;
+    console.log("oi ayrton");
+    console.log($scope.ocorrencias);
+    // var demandas = [];
+    //     $http.get('http://10.21.0.137/20131011110380/api/demanda').
+    //         then(function(responsedemanda) {
+    //             demandas = responsedemanda.data;
+    //         });
+    for (var i = 0; i < $scope.ocorrencias.length; i++)
+    {
+      setDemanda($scope.ocorrencias[i].demanda, i);
+      setLocal($scope.ocorrencias[i].local, i);
+
+
+      // var local = $.grep(locais, function(e){ return e.id == $scopre.ocorrencias[i].local; });
+      // $scope.ocorrencias[i].local = local;
+    }
+  });
+})
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
